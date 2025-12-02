1cc076baef59169cba9e97372ad3100fa9eb2822
rhffna928 <rhffna928@naver.com>
2025-12-02 18:32:59 +0900

    에러처리

---
+++ src/main/java/com/samhwan/dashboard/exception/BadRequestExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.samhwan.dashboard.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.samhwan.dashboard.dto.response.ResponseDto;
+
+@RestControllerAdvice
+public class BadRequestExceptionHandler {
+
+    @ExceptionHandler({MethodArgumentNotValidException.class, HttpMessageNotReadableException.class})
+    public ResponseEntity<ResponseDto> validationExceptionHandler(Exception exception){
+        return ResponseDto.validationFailed();
+    }
+}
