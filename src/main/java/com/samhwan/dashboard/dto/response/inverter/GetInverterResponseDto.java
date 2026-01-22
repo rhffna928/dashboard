5251dc61fc600cfb1ab2d76a48c5b0c5720f35e1
rhffna928 <rhffna928@naver.com>
2026-01-22 20:54:54 +0900

    알람 데이터 추가

---
+++ src/main/java/com/samhwan/dashboard/dto/response/inverter/GetInverterResponseDto.java
@@ -0,0 +1,94 @@
+package com.samhwan.dashboard.dto.response.inverter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.samhwan.dashboard.common.ResponseCode;
+import com.samhwan.dashboard.common.ResponseMessage;
+import com.samhwan.dashboard.dto.response.ResponseDto;
+import com.samhwan.dashboard.entity.Inverter;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class GetInverterResponseDto extends ResponseDto{
+
+    
+    private final List<InverterSummary> inverters;
+
+    private GetInverterResponseDto(List<InverterSummary> inverters) {
+        super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
+        this.inverters = inverters;
+    }
+    public static ResponseEntity<GetInverterResponseDto> success(List<Inverter> inverters){
+        List<InverterSummary> list = inverters.stream()
+                    .map(InverterSummary::fromEntity)
+                    .collect(Collectors.toList());
+        GetInverterResponseDto result =  new GetInverterResponseDto(list);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    @Getter
+    @Builder
+    public static class InverterSummary {
+        private Integer id;
+        private Integer plantId;
+        private Integer invId;
+
+        private String invStatus;
+        private String invFault;
+
+        private Double inVolt;
+        private Double inCurrent;
+        private Double inPower;
+
+        private Double outVolt1;
+        private Double outVolt2;
+        private Double outVolt3;
+
+        private Double outCurrent1;
+        private Double outCurrent2;
+        private Double outCurrent3;
+
+        private Double outPower;
+        private Double hz;
+
+        private Double todayGen;
+        private Double totalGen;
+
+        private LocalDateTime recvTime;
+        private LocalDateTime regdate;
+    
+
+        public static InverterSummary fromEntity(Inverter e) {
+            return InverterSummary.builder()
+                .id(e.getId())
+                .plantId(e.getPlantId())
+                .invId(e.getInvId())
+                .invStatus(e.getInvStatus())
+                .invFault(e.getInvFault())
+                .inVolt(e.getInVolt())
+                .inCurrent(e.getInCurrent())
+                .inPower(e.getInPower())
+                .outVolt1(e.getOutVolt1())
+                .outVolt2(e.getOutVolt2())
+                .outVolt3(e.getOutVolt3())
+                .outCurrent1(e.getOutCurrent1())
+                .outCurrent2(e.getOutCurrent2())
+                .outCurrent3(e.getOutCurrent3())
+                .outPower(e.getOutPower())
+                .hz(e.getHz())
+                .todayGen(e.getTodayGen())
+                .totalGen(e.getTotalGen())
+                .recvTime(e.getRecvTime())
+                .regdate(e.getRegdate())
+                .build();
+        }
+    }
+
+}
