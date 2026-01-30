ef2dc870f9df16445e37d9406f083086d50ccd0a
rhffna928 <rhffna928@naver.com>
2026-01-30 10:41:29 +0900

    불필요한 참조 삭제

--- src/main/java/com/samhwan/dashboard/service/implement/InverterServiceImpl.java
+++ src/main/java/com/samhwan/dashboard/service/implement/InverterServiceImpl.java
@@ -2,7 +2,6 @@ package com.samhwan.dashboard.service.implement;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -10,10 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.samhwan.dashboard.dto.response.inverter.GetInverterResponseDto;
-import com.samhwan.dashboard.dto.response.inverter.GetUserInverterList2ResponseDto;
 import com.samhwan.dashboard.dto.response.inverter.GetUserInverterResponseDto;
 import com.samhwan.dashboard.entity.Inverter;
-import com.samhwan.dashboard.entity.InverterList2;
 import com.samhwan.dashboard.repository.InverterRepository;
 import com.samhwan.dashboard.service.InverterInterfaceService;
 
